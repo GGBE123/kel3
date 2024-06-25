60dd21cccc85f6747f1609b6bd99de4c24083dea
GGBE123 <115924499+GGBE123@users.noreply.github.com>
2024-06-25 07:48:42 +0700

    Add files via upload

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,7 +2,7 @@
 <!-- Facts Start -->
 
 <div class="container-fluid facts py-5 pt-lg-0">
-    <div class="container py-5 pt-lg-0">
+    <div class="container py-10 pt-lg-0">
         <div class="row gx-0">
             <div class="col-lg-4 wow zoomIn" data-wow-delay="0.1s">
                 <div class="bg-primary shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
@@ -44,72 +44,78 @@
 <!-- Facts Start -->
 
 
-<!-- Testimonial Start -->
-<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-    <div class="container py-5">
-        <div class="section-title text-center position-relative pb-3 mb-4 mx-auto" style="max-width: 600px;">
-            <h5 class="fw-bold text-primary text-uppercase">Rekomendasi</h5>
-            <h1 class="mb-0">What Our Clients Say About Our Digital Services</h1>
-        </div>
-        <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.6s">
-            <div class="testimonial-item bg-light my-4">
-                <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-                    <img src="<?= base_url('assets/');?>img/testimonial-1.jpg" style="width: 60px; height: 60px;">
-                    <div class="ps-4">
-                        <h4 class="text-primary mb-1">Praktikum Administrasi Jaringan Komputer </h4>
-                        <small class="text-uppercase">Rahmat Suhatman, Muhammad Arif Fadhly Ridha	</small>
+    <!-- Testimonial Start -->
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="section-title text-center position-relative pb-3 mb-4 mx-auto" style="max-width: 600px;">
+                <h5 class="fw-bold text-primary text-uppercase">Recommendation</h5>
+                <h1 class="mb-0">LIBRARY BOOKS</h1>
+            </div>
+            <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.6s">
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Workshop Pengembangan Web Lanjut</h4>
+                            <small class="text-uppercase">Anggy Trisnadoli, Muhammad Ihsan Zul	</small>
+                        </div>
                     </div>
-                </div>
-                <div class="pt-4 pb-5 px-5">
-                    <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="">
-                </div>
-                <a class="btn btn-success" href="<?= base_url('DetailBuku/1');?>">
+                    <div class="pt-4 pb-5 px-5">
+                    <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="Book 1" class="book-image">
+                    
+                    </div>
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/14">
                                         Detail
-                                    </a>
-            </div>
-            <div class="testimonial-item bg-light my-4">
-                <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-                    <img class="img-fluid rounded" src="<?= base_url('assets/');?>img/testimonial-2.jpg" style="width: 60px; height: 60px;">
-                    <div class="ps-4">
-                        <h4 class="text-primary mb-1">nama buku</h4>
-                        <small class="text-uppercase">Profession</small>
+                    </a>
+                </div>
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Pemrograman Berorientasi Objek</h4>
+                            <small class="text-uppercase">	Juni Nurma Sari, Silvana Rasio Henim</small>
+                        </div>
                     </div>
-                </div>
-                <div class="pt-4 pb-5 px-5">
-                <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="">
-                </div>
-                <a class="btn btn-success" href="<?= base_url('DetailBuku/2');?>">
+                    <div class="pt-4 pb-5 px-5">
+                    <img src="https://isbn.lib.pcr.ac.id/storage/nSPRKEmQQAI9BEWfuFzqs3X0msBNs9-metaQ292ZXIgSk5TIFBCTy5qcGc=-.jpg" alt="" width="200px" height="475px">
+                    
+                    </div>
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/31">
                                         Detail
-                                    </a>
-            </div>
-            <div class="testimonial-item bg-light my-4">
-                <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-                    <img class="img-fluid rounded" src="<?= base_url('assets/');?>img/testimonial-3.jpg" style="width: 60px; height: 60px;">
-                    <div class="ps-4">
-                        <h4 class="text-primary mb-1">nama buku</h4>
-                        <small class="text-uppercase">Profession</small>
+                    </a>
+                </div>
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Workshop Basis Data Lanjut : Teori Dan Praktik Basis Data Dengan Oracle</h4>
+                            <small class="text-uppercase">Muhammad Ihsan Zul, Khairul Umam Syaliman</small>
+                        </div>
                     </div>
-                </div>
-                <div class="pt-4 pb-5 px-5">
-                <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="">
-                </div>
-            </div>
-            <div class="testimonial-item bg-light my-4">
-                <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-                    <img class="img-fluid rounded" src="<?= base_url('assets/');?>img/testimonial-4.jpg" style="width: 60px; height: 60px;">
-                    <div class="ps-4">
-                        <h4 class="text-primary mb-1">nama buku</h4>
-                        <small class="text-uppercase">Profession</small>
+                    <div class="pt-4 pb-5 px-5">
+                    <img src="https://isbn.lib.pcr.ac.id/storage/lCj3pS6BQbbjlMxFusLFIkzTYe9pPJ-metaQ292ZXIgTUlaIEJETC5qcGc=-.jpg" alt="Book 1" class="book-image">
+                    
                     </div>
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/34">
+                                        Detail
+                    </a>
                 </div>
-                <div class="pt-4 pb-5 px-5">
-                <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="">
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Pengukuran Besaran Listrik</h4>
+                            <small class="text-uppercase">Putri Madona, Retno Tri Wahyuni</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    <img src="https://isbn.lib.pcr.ac.id/storage/VFdrhLDRAPfL99N1fi517YO2aM3iS6-metaQ292ZXIgTUROIFBCTC5qcGc=-.jpg" alt="Book 1" class="book-image">
+                    
+                    </div>
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/13">
+                                        Detail
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Testimonial End -->
+    <!-- Testimonial End -->
 
 
 <!-- Vendor Start -->
@@ -131,3 +137,5 @@
     </div>
 </div>
 <!-- Vendor End -->
+
+    
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -97,11 +97,11 @@
         <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="<?= base_url('assets/');?>img/carousel-1.jpg" alt="Image">
+                    <img class="w-100" src="<?= base_url('assets/');?>img/depanpcr.jpg" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
-                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
-                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">Creative & Innovative Digital Solution</h1>
+                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Request ISBN PCR</h5>
+                            <h2 class="display-1 text-white mb-md-4 animated zoomIn"> International Standard Book Number</h2>
                             <a href="<?= base_url('Auth');?>" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Request No ISBN</a>
                             <a href="<?= base_url('list_buku');?>" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">List Catalog</a>
                         </div>
