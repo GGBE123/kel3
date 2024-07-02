f68cfc4054de3aa54e0bcd9b6bcb276037212d41
GGBE123 <115924499+GGBE123@users.noreply.github.com>
2024-07-02 07:51:38 +0700

    Add files via upload

--- application/views/LayoutTemplate/navbar.php
+++ application/views/LayoutTemplate/navbar.php
@@ -3,40 +3,33 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Startup - Startup Website Template</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
 
-    <!-- Favicon -->
-    <link href="<?= base_url('assets/'); ?>img/favicon.ico" rel="icon">
-
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="<?= base_url('assets/'); ?>lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/'); ?>lib/animate/animate.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/');?>lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/');?>lib/animate/animate.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="<?= base_url('assets/'); ?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/');?>css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
+    <link href="<?= base_url('assets/');?>css/style.css" rel="stylesheet">
 </head>
 
 <body>
     <!-- Spinner Start -->
-    <div id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner"></div>
     </div>
     <!-- Spinner End -->
@@ -47,24 +40,16 @@
         <div class="row gx-0">
             <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
                 <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <small class="me-3 text-light"><i class="fa fa-map-marker-alt me-2"></i>Politeknik Caltex Riau Jl.
-                        Umban Sari No.1, Umban Sari, Kec. Rumbai, Kota Pekanbaru, Riau 28265</small>
-
+                    <small class="me-3 text-light"><i class="fa fa-map-marker-alt me-2"></i>Politeknik Caltex Riau Jl. Umban Sari No.1, Umban Sari, Kec. Rumbai, Kota Pekanbaru, Riau 28265</small>
+                  
                 </div>
             </div>
             <div class="col-lg-4 text-center text-lg-end">
                 <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-                        href="https://twitter.com/PolicaltexRiau"><i class="fab fa-twitter fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-                        href="https://www.facebook.com/Politeknik.Caltex.Riau"><i
-                            class="fab fa-facebook-f fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-                        href="https://www.youtube.com/channel/UC_xsN7_lBMY_m_C8a_34uZg"><i
-                            class="fab fa-youtube fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
-                        href="https://www.instagram.com/politeknikcaltexriau/"><i
-                            class="fab fa-instagram fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"  href="https://twitter.com/PolicaltexRiau"><i class="fab fa-twitter fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"  href="https://www.facebook.com/Politeknik.Caltex.Riau"><i class="fab fa-facebook-f fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="https://www.youtube.com/channel/UC_xsN7_lBMY_m_C8a_34uZg"><i class="fab fa-youtube fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="https://www.instagram.com/politeknikcaltexriau/"><i class="fab fa-instagram fw-normal"></i></a>
                 </div>
             </div>
         </div>
@@ -76,43 +61,39 @@
     <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0">
             <a href="#" class="navbar-brand p-0">
-                <img class="img-fluid" src="<?= base_url('assets/') ?>img/logo_pcr.png" alt="Theme-Logo"
-                    style="width: 300px;" />
+                <h1 class="m-0"><i class="fa fa-user-tie me-2"></i>POLITEKNIK CALTEX RIAU</h1>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav ms-auto py-0">
-                    <a href="<?= base_url('home'); ?>" class="nav-item nav-link">Home</a>
-                    <a href="<?= base_url('list_Buku'); ?>" class="nav-item nav-link">List Catalog</a>
+                    <a href="<?= base_url('home');?>" class="nav-item nav-link">Home</a>
+                    <a href="<?= base_url('list_Buku');?>" class="nav-item nav-link">Catalog List</a>
+                    </div>
+                    </div>
+                    <a href="<?= base_url('AdminLogin');?>" class="nav-item nav-link">
+                <h5 class="m-0 text-primary"><i class="fa fa-user-tie me-2"></i>Admin Login</h5>
+            </a>
                 </div>
             </div>
-            <a href="<?= base_url('AdminLogin'); ?>" class="nav-item nav-link">
-                <h3 class="m-0"><i class="fa fa-user-tie me-2"></i>Admin Login</h3>
-            </a>
-    </div>
-    </div>
-    </nav>
+        </nav>
 
-    <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="w-100" src="<?= base_url('assets/'); ?>img/depanpcr.jpg" alt="Image">
-                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                    <div class="p-3" style="max-width: 900px;">
-                        <h5 class="text-white text-uppercase mb-3 animated slideInDown">Request ISBN PCR</h5>
-                        <h2 class="display-1 text-white mb-md-4 animated zoomIn"> International Standard Book Number
-                        </h2>
-                        <a href="<?= base_url('PenulisLogin'); ?>"
-                            class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Login</a>
-                        <a href="<?= base_url('list_buku'); ?>"
-                            class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">List Catalog</a>
+        <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="<?= base_url('assets/');?>img/depanpcr.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Request ISBN PCR</h5>
+                            <h2 class="display-1 text-white mb-md-4 animated zoomIn"> International Standard Book Number</h2>
+                            <a href="<?= base_url('PenulisLogin');?>" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Login</a>
+                            <a href="<?= base_url('list_buku');?>" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">List Catalog</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     </div>
     <!-- Navbar & Carousel End -->
     <!-- Full Screen Search Start -->
@@ -120,13 +101,11 @@
         <div class="modal-dialog modal-fullscreen">
             <div class="modal-content" style="background: rgba(9, 30, 62, .7);">
                 <div class="modal-header border-0">
-                    <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
+                    <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body d-flex align-items-center justify-content-center">
                     <div class="input-group" style="max-width: 600px;">
-                        <input type="text" class="form-control bg-transparent border-primary p-3"
-                            placeholder="Type search keyword">
+                        <input type="text" class="form-control bg-transparent border-primary p-3" placeholder="Type search keyword">
                         <button class="btn btn-primary px-4"><i class="bi bi-search"></i></button>
                     </div>
                 </div>
