91c457d534f66b0c6dece5db20510ccf13bd5235
nevo1945 <nevo23ti@mahasiswa.pcr.ac.id>
2024-07-08 11:14:42 +0700

    Update Logout.php

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -1,18 +1,30 @@
 <?php
+// Definisi class Logout yang merupakan controller untuk proses logout
 class Logout extends CI_Controller
 {
 
+    // Method index untuk proses logout dan memberikan pesan berhasil
     public function index()
     {
+        // Menghapus data session 'email'
         $this->session->unset_userdata('email');
+        
+        // Menyimpan pesan flash untuk ditampilkan setelah redirect
         $this->session->set_flashdata('login_message','<div class="alert alert-success text-center" role="alert">Berhasil Keluar</div>');
+        
+        // Melakukan redirect ke halaman 'home'
         redirect('home');
-
     }
+
+    // Method redirect untuk proses logout tanpa menyimpan pesan flash
     public function redirect()
     {
+        // Menghapus data session 'email'
         $this->session->unset_userdata('email');
+        
+        // Melakukan redirect ke halaman 'home'
         redirect('home');
-
     }
+
 }
+?>
