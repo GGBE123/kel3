589750d3f7e1184ea6c3a3347563ac63999cc31c
nevo1945 <nevo23ti@mahasiswa.pcr.ac.id>
2024-07-08 11:17:11 +0700

    Update Penulis_model.php

--- application/models/Penulis_model.php
+++ application/models/Penulis_model.php
@@ -1,20 +1,24 @@
 <?php
 class Penulis_model extends CI_Model
 {
-    private $PENULIS_USER = 'penulis';
+    private $PENULIS_USER = 'penulis'; // Nama tabel dalam database
 
     public function get_user_data($email)
     {
-        return $this->db->get_where($this->PENULIS_USER, ['email'=> $email])->row_array();
+        // Mengambil data pengguna berdasarkan email
+        return $this->db->get_where($this->PENULIS_USER, ['email' => $email])->row_array();
     }
 
     public function check_user($email)
     {
+        // Memeriksa keberadaan pengguna berdasarkan email
         $user = $this->db->get_where($this->PENULIS_USER, ['email' => $email])->row_array();
 
         if ($user) {
+            // Jika pengguna ditemukan, kembalikan data pengguna
             return $user;
         } else {
+            // Jika pengguna tidak ditemukan, kembalikan false
             return false;
         }
     }
