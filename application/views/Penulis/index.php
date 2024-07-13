d3427d8bd4f1e4e6a5ef18e33dd262f74137e433
dafiswahyuni <dafis23ti@mahasiswa.pcr.ac.id>
2024-07-13 21:58:58 +0700

    Update index.php

--- application/views/Penulis/index.php
+++ application/views/Penulis/index.php
@@ -5,17 +5,8 @@
             <div class="row align-items-center mt-5">
                 <div class="col-md-8">
                     <div class="page-header-title">
-                        <p class="m-b-0">Dashboard menu</p>
+                        <p class="m-b-0">Dashboard User</p>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <ul class="breadcrumb-title">
-                        <li class="breadcrumb-item">
-                            <a href="index.html"> <i class="fa fa-home"></i> </a>
-                        </li>
-                        <li class="breadcrumb-item"><a href="#!">Dashboard</a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
@@ -35,7 +26,7 @@
                                     <div class="row align-items-center">
                                         <div class="col-8">
                                             <h4 class="text-c-purple"><?= isset($countProses) ? $countProses : '0'; ?></h4>
-                                            <h6 class="text-muted m-b-0">Dalam Antrian</h6>
+                                            <h6 class="text-muted m-b-0">Waiting List</h6>
                                         </div>
                                         <div class="col-4 text-right">
                                             <i class="fa fa-bar-chart f-28"></i>
@@ -45,10 +36,8 @@
                                 <div class="card-footer bg-c-purple">
                                     <div class="row align-items-center">
                                         <div class="col-9">
-                                            <p class="text-white m-b-0">% change</p>
                                         </div>
                                         <div class="col-3 text-right">
-                                            <i class="fa fa-line-chart text-white f-16"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -60,20 +49,18 @@
                                     <div class="row align-items-center">
                                         <div class="col-8">
                                             <h4 class="text-c-green"><?= isset($countReview) ? $countReview : '0'; ?></h4>
-                                            <h6 class="text-muted m-b-0">Sedang Direview</h6>
+                                            <h6 class="text-muted m-b-0">Being Reviewed</h6>
                                         </div>
                                         <div class="col-4 text-right">
-                                            <i class="fa fa-file-text-o f-28"></i>
+                                            <i class="fa fa-bar-chart f-28"></i>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer bg-c-green">
                                     <div class="row align-items-center">
                                         <div class="col-9">
-                                            <p class="text-white m-b-0">% change</p>
                                         </div>
                                         <div class="col-3 text-right">
-                                            <i class="fa fa-line-chart text-white f-16"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -85,20 +72,18 @@
                                     <div class="row align-items-center">
                                         <div class="col-8">
                                             <h4 class="text-c-red"><?= isset($countDiajukanIsbn) ? $countDiajukanIsbn : '0'; ?></h4>
-                                            <h6 class="text-muted m-b-0">Sedang diajukan ke ISBN</h6>
+                                            <h6 class="text-muted m-b-0">Processed by PERPUSNAS</h6>
                                         </div>
                                         <div class="col-4 text-right">
-                                            <i class="fa fa-calendar-check-o f-28"></i>
+                                            <i class="fa fa-bar-chart f-28"></i>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer bg-c-red">
                                     <div class="row align-items-center">
                                         <div class="col-9">
-                                            <p class="text-white m-b-0">% change</p>
                                         </div>
                                         <div class="col-3 text-right">
-                                            <i class="fa fa-line-chart text-white f-16"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -110,20 +95,18 @@
                                     <div class="row align-items-center">
                                         <div class="col-8">
                                             <h4 class="text-c-blue"><?= isset($countDiterima) ? $countDiterima : '0'; ?></h4>
-                                            <h6 class="text-muted m-b-0">Diterima</h6>
+                                            <h6 class="text-muted m-b-0">Accepted</h6>
                                         </div>
                                         <div class="col-4 text-right">
-                                            <i class="fa fa-hand-o-down f-28"></i>
+                                            <i class="fa fa-bar-chart f-28"></i>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer bg-c-blue">
                                     <div class="row align-items-center">
                                         <div class="col-9">
-                                            <p class="text-white m-b-0">% change</p>
                                         </div>
                                         <div class="col-3 text-right">
-                                            <i class="fa fa-line-chart text-white f-16"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -135,20 +118,18 @@
                                     <div class="row align-items-center">
                                         <div class="col-8">
                                             <h4 class="text-c-blue"><?= isset($countDitolak) ? $countDitolak : '0'; ?></h4>
-                                            <h6 class="text-muted m-b-0">Ditolak</h6>
+                                            <h6 class="text-muted m-b-0">Denied</h6>
                                         </div>
                                         <div class="col-4 text-right">
-                                            <i class="fa fa-hand-o-down f-28"></i>
+                                            <i class="fa fa-bar-chart f-28"></i>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card-footer bg-c-blue">
                                     <div class="row align-items-center">
                                         <div class="col-9">
-                                            <p class="text-white m-b-0">% change</p>
                                         </div>
                                         <div class="col-3 text-right">
-                                            <i class="fa fa-line-chart text-white f-16"></i>
                                         </div>
                                     </div>
                                 </div>
