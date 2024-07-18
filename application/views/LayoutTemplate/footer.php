730070b8f252505278ea8cb3c22de22348f8929b
dafiswahyuni <dafis23ti@mahasiswa.pcr.ac.id>
2024-07-18 19:22:41 +0700

    Update footer.php

--- application/views/LayoutTemplate/footer.php
+++ application/views/LayoutTemplate/footer.php
@@ -2,33 +2,39 @@
 <div class="container-fluid bg-dark text-light mt-5 wow fadeInUp" data-wow-delay="0.1s">
     <div class="container">
         <div class="row gx-5">
+            <!-- Bagian kolom lokasi dengan peta -->
             <div class="col-lg-4 col-md-6 footer-about">
                 <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
                     <a>
                         <h1 class="m-0 text-white">LOCATION</h1>
                     </a>
+                    <!-- IFrame untuk menampilkan peta lokasi -->
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15958.504755926126!2d101.4225703!3d0.5622920499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5ab67086f2e89%3A0x65a24264fec306bb!2sPoliteknik%20Caltex%20Riau!5e0!3m2!1sen!2sid!4v1719198522076!5m2!1sen!2sid" width="350" height="370" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
+            <!-- Kolom informasi kontak -->
             <div class="col-lg-8 col-md-6">
                 <div class="row gx-5">
+                    <!-- Bagian informasi kontak -->
                     <div class="col-lg-4 col-md-12 pt-5 mb-5">
                         <div class="section-title section-title-sm position-relative pb-3 mb-4">
                             <h3 class="text-light mb-0">Contact Us</h3>
                         </div>
+                        <!-- Detail kontak -->
                         <div class="d-flex mb-2">
                             <i class="bi bi-geo-alt text-primary me-2"></i>
-                            <p  class="m-0 text-white">Politeknik Caltex Riau
+                            <p class="m-0 text-white">Politeknik Caltex Riau
                                 Jl. Umban Sari No.1, Umban Sari, Kec. Rumbai, Kota Pekanbaru, Riau 28265</p>
                         </div>
                         <div class="d-flex mb-2">
                             <i class="bi bi-envelope-open text-primary me-2"></i>
-                            <p  class="m-0 text-white"> pcr@pcr.ac.id</p>
+                            <p class="m-0 text-white"> pcr@pcr.ac.id</p>
                         </div>
                         <div class="d-flex mb-2">
                             <i class="bi bi-telephone text-primary me-2"></i>
                             <p class="m-0 text-white">(0761) - 53939 atau 0811 758 0101</p>
                         </div>
+                        <!-- Tombol media sosial -->
                         <div class="d-flex mt-4">
                             <a class="btn btn-primary btn-square me-2" href="https://twitter.com/PolicaltexRiau"><i class="fab fa-twitter fw-normal"></i></a>
                             <a class="btn btn-primary btn-square me-2" href="https://www.facebook.com/Politeknik.Caltex.Riau"><i class="fab fa-facebook-f fw-normal"></i></a>
@@ -36,10 +42,12 @@
                             <a class="btn btn-primary btn-square" href="https://www.instagram.com/politeknikcaltexriau/"><i class="fab fa-instagram fw-normal"></i></a>
                         </div>
                     </div>
+                    <!-- Informasi sistem dan layanan -->
                     <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
                         <div class="section-title section-title-sm position-relative pb-3 mb-4">
                             <h3 class="text-light mb-0">Information System</h3>
                         </div>
+                        <!-- Daftar tautan informasi sistem -->
                         <div class="link-animated d-flex flex-column justify-content-start">
                             <a class="text-light mb-2" href="https://jurnal.pcr.ac.id/"><i class="bi bi-arrow-right text-primary me-2"></i>Jurnal PCR</a>
                             <a class="text-light mb-2" href="https://mahasiswa.pcr.ac.id/"><i class="bi bi-arrow-right text-primary me-2"></i>SIAK Mahasiswa</a>
@@ -50,10 +58,12 @@
                             <a class="text-light mb-2" href="http://opac.lib.pcr.ac.id/"><i class="bi bi-arrow-right text-primary me-2"></i>Perpustakaan</a>
                         </div>
                     </div>
+                    <!-- Daftar layanan -->
                     <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
                         <div class="section-title section-title-sm position-relative pb-3 mb-4">
                             <h3 class="text-light mb-0">Services</h3>
                         </div>
+                        <!-- Daftar tautan layanan -->
                         <div class="link-animated d-flex flex-column justify-content-start">
                             <a class="text-light mb-2" href="http://baak.pcr.ac.id/"><i class="bi bi-arrow-right text-primary me-2"></i>BAAK PCR</a>
                             <a class="text-light mb-2" href="http://scc.pcr.ac.id/"><i class="bi bi-arrow-right text-primary me-2"></i>Career Center</a>
@@ -69,13 +79,13 @@
         </div>
     </div>
 </div>
+<!-- Bagian footer dengan hak cipta -->
 <div class="container-fluid text-white" style="background: #061429;">
     <div class="container text-center">
         <div class="row justify-content-end">
             <div class="col-lg-8 col-md-6">
                 <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
-                    <p  class="m-0 text-white">&copy; <a class="text-white border-bottom" href="https://pcr.ac.id/">Politeknik Caltex Riau</a>. All Rights Reserved.
-                    </p>
+                    <p class="m-0 text-white">&copy; <a class="text-white border-bottom" href="https://pcr.ac.id/">Politeknik Caltex Riau</a>. All Rights Reserved.</p>
                 </div>
             </div>
         </div>
@@ -84,11 +94,11 @@
 <!-- Footer End -->
 
 
-<!-- Back to Top -->
+<!-- Tombol Kembali ke Atas -->
 <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
 
 
-<!-- JavaScript Libraries -->
+<!-- Pustaka JavaScript -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -98,7 +108,7 @@
 <script src="<?= base_url('assets/'); ?>lib/counterup/counterup.min.js"></script>
 <script src="<?= base_url('assets/'); ?>lib/owlcarousel/owl.carousel.min.js"></script>
 
-<!-- Template Javascript -->
+<!-- Skrip JavaScript Template -->
 <script src="<?= base_url('assets/'); ?>js/main.js"></script>
 </body>
 
