0d638cd84d109f6b155c308f9fa1f2424d0b1270
dafiswahyuni <dafis23ti@mahasiswa.pcr.ac.id>
2024-07-18 19:12:21 +0700

    Update index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,77 +1,89 @@
- <!-- Testimonial Start -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>ISBN PCR</title>
+    <!-- Favicon -->
+    <link rel="icon" href="<?= base_url('assets/img/LogoProj.png')?>"> <!-- Menambahkan favicon untuk halaman -->
+
+    <!-- Mulai bagian testimonial -->
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-4 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Recommendation</h5>
-                <h1 class="mb-0">LIBRARY BOOKS</h1>
+                <h5 class="fw-bold text-primary text-uppercase">Recommendation</h5> <!-- Judul rekomendasi buku -->
+                <h1 class="mb-0">LIBRARY BOOKS</h1> <!-- Judul koleksi buku perpustakaan -->
             </div>
+            <!-- Carousel testimonial buku -->
             <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.6s">
+                <!-- Item testimonial buku pertama -->
                 <div class="testimonial-item bg-light my-4">
                     <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
                         <div class="ps-4">
-                            <h4 class="text-primary mb-1">Workshop Pengembangan Web Lanjut</h4>
-                            <small class="text-uppercase">Anggy Trisnadoli, Muhammad Ihsan Zul	</small>
+                            <h4 class="text-primary mb-1">Workshop Pengembangan Web Lanjut</h4> <!-- Judul buku -->
+                            <small class="text-uppercase">Anggy Trisnadoli, Muhammad Ihsan Zul</small> <!-- Penulis buku -->
                         </div>
                     </div>
                     <div class="pt-4 pb-5 px-5">
-                    <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="Book 1" class="book-image">
-                    
+                        <img src="https://isbn.lib.pcr.ac.id/storage/8klUDqCN2ajXuYzyLHrL7hBR7pM3W1-metaQ292ZXIgWW9hbmRhIEVtYmVkZGVkLmpwZw==-.jpg" alt="Book 1" class="book-image"> <!-- Gambar buku -->
                     </div>
-                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/14">
-                                        Detail
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/14"> <!-- Tombol detail -->
+                        Detail
                     </a>
                 </div>
+                <!-- Item testimonial buku kedua -->
                 <div class="testimonial-item bg-light my-4">
                     <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
                         <div class="ps-4">
-                            <h4 class="text-primary mb-1">Pemrograman Berorientasi Objek</h4>
-                            <small class="text-uppercase">	Juni Nurma Sari, Silvana Rasio Henim</small>
+                            <h4 class="text-primary mb-1">Pemrograman Berorientasi Objek</h4> <!-- Judul buku -->
+                            <small class="text-uppercase">Juni Nurma Sari, Silvana Rasio Henim</small> <!-- Penulis buku -->
                         </div>
                     </div>
                     <div class="pt-4 pb-5 px-5">
-                    <img src="https://isbn.lib.pcr.ac.id/storage/nSPRKEmQQAI9BEWfuFzqs3X0msBNs9-metaQ292ZXIgSk5TIFBCTy5qcGc=-.jpg" alt="" width="200px" height="475px">
-                    
+                        <img src="https://isbn.lib.pcr.ac.id/storage/nSPRKEmQQAI9BEWfuFzqs3X0msBNs9-metaQ292ZXIgSk5TIFBCTy5qcGc=-.jpg" alt="Book 2" width="200px" height="475px"> <!-- Gambar buku -->
                     </div>
-                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/31">
-                                        Detail
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/31"> <!-- Tombol detail -->
+                        Detail
                     </a>
                 </div>
+                <!-- Item testimonial buku ketiga -->
                 <div class="testimonial-item bg-light my-4">
                     <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
                         <div class="ps-4">
-                            <h4 class="text-primary mb-1">Rajut gagasan : dari gagasan menjadi rancangan</h4>
-                            <small class="text-uppercase">Syaifuddin Abdullah</small>
+                            <h4 class="text-primary mb-1">Rajut gagasan : dari gagasan menjadi rancangan</h4> <!-- Judul buku -->
+                            <small class="text-uppercase">Syaifuddin Abdullah</small> <!-- Penulis buku -->
                         </div>
                     </div>
                     <div class="pt-4 pb-5 px-5">
-                    <img src="https://isbn.lib.pcr.ac.id/storage/p7cDmfHnVDNihOPACDF7JuR68wXdu9-metaQ292ZXIgU0EgUmFqdXQuanBn-.jpg"alt="Book 1" class="book-image">
-                    
+                        <img src="https://isbn.lib.pcr.ac.id/storage/p7cDmfHnVDNihOPACDF7JuR68wXdu9-metaQ292ZXIgU0EgUmFqdXQuanBn-.jpg" alt="Book 3" class="book-image"> <!-- Gambar buku -->
                     </div>
-                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/26">
-                                        Detail
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/26"> <!-- Tombol detail -->
+                        Detail
                     </a>
                 </div>
+                <!-- Item testimonial buku keempat -->
                 <div class="testimonial-item bg-light my-4">
                     <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
                         <div class="ps-4">
-                            <h4 class="text-primary mb-1">Workshop Enterprise Resource Planning</h4>
-                            <small class="text-uppercase">Nina Fadilah Najwa, Istianah Muslim</small>
+                            <h4 class="text-primary mb-1">Workshop Enterprise Resource Planning</h4> <!-- Judul buku -->
+                            <small class="text-uppercase">Nina Fadilah Najwa, Istianah Muslim</small> <!-- Penulis buku -->
                         </div>
                     </div>
                     <div class="pt-4 pb-5 px-5">
-                    <img src="https://isbn.lib.pcr.ac.id/storage/fR0zOuwJncNfmONEv0elSop8ECpDEi-metaQ292ZXIgTklOQSBFbnRlcnByaXNlLmpwZw==-.jpg"alt="Book 1" class="book-image">
-                    
+                        <img src="https://isbn.lib.pcr.ac.id/storage/fR0zOuwJncNfmONEv0elSop8ECpDEi-metaQ292ZXIgTklOQSBFbnRlcnByaXNlLmpwZw==-.jpg" alt="Book 4" class="book-image"> <!-- Gambar buku -->
                     </div>
-                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/37">
-                                        Detail
+                    <a class="btn btn-success" href="https://isbn.lib.pcr.ac.id/detail/37"> <!-- Tombol detail -->
+                        Detail
                     </a>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Testimonial End -->
+    <!-- Selesai bagian testimonial -->
+</head>
 
+<body>
+    <!-- Body content -->
+</body>
 
-
-
-    
+</html>
+F
