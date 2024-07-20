c5170383c793fcc09b154e1e9d470ca92c4520d3
dafiswahyuni <dafis23ti@mahasiswa.pcr.ac.id>
2024-07-20 19:42:52 +0700

    Update data_penulis.php

--- application/views/Admin/master_data/data_penulis.php
+++ application/views/Admin/master_data/data_penulis.php
@@ -1,5 +1,6 @@
-<!-- Page-header start -->
-<div class="page-header">
+<div class="pcoded-content">
+    <!-- Page-header start -->
+    <div class="page-header">
         <div class="page-block">
             <div class="row align-items-center mt-5">
                 <div class="col-md-8">
@@ -12,8 +13,7 @@
                         <li class="breadcrumb-item">
                             <a href="index.html"> <i class="fa fa-home"></i> </a>
                         </li>
-                        <li class="breadcrumb-item"><a href="#!">Dashboard</a>
-                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Dashboard</a></li>
                     </ul>
                 </div>
             </div>
@@ -21,61 +21,34 @@
     </div>
     <!-- Page-header end -->
     <div class="card">
-    <div class="card-header">
-        <h5 class="mb-0">Daftar Akun Penulis</h5>
-    </div>
-    <div class="card-block table-border-style">
-        <div class="table-responsive">
-            <table class="table table-striped table-bordered">
-                <thead  class="bg-dark text-white">
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Sedang proses</td>
-                        <td>
-                            <button class="btn btn-sm btn-primary">Tambahkan</button>
-                            <button class="btn btn-sm btn-info">Update</button>
-                            <button class="btn btn-sm btn-danger">Hapus</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>Sedang direview editor</td>
-                        <td>
-                            <button class="btn btn-sm btn-primary">Tambahkan</button>
-                            <button class="btn btn-sm btn-info">Update</button>
-                            <button class="btn btn-sm btn-danger">Hapus</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Larry</td>
-                        <td>The Bird</td>
-                        <td>@twitter</td>
-                        <td>Active</td>
-                        <td>
-                            <button class="btn btn-sm btn-primary">Tambahkan</button>
-                            <button class="btn btn-sm btn-info">Update</button>
-                            <button class="btn btn-sm btn-danger">Hapus</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="card-header">
+            <h5 class="mb-0">Daftar Akun Penulis</h5>
+        </div>
+        <div class="card-block table-border-style">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead class="bg-dark text-white">
+                        <tr>
+                            <th>#</th>
+                            <th>NIP/M</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($penulis as $index => $p): ?>
+                        <tr>
+                            <td><?= $index + 1 ?></td>
+                            <td><?= $p->nip_m ?></td>
+                            <td><?= $p->nama ?></td>
+                            <td><?= $p->email ?></td>
+                            <td><?= $p->role ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
