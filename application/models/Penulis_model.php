f6be132c531d6a08e12853fbf848037fac884ce1
dafiswahyuni <dafis23ti@mahasiswa.pcr.ac.id>
2024-07-20 19:57:52 +0700

    Update Penulis_model.php

--- application/models/Penulis_model.php
+++ application/models/Penulis_model.php
@@ -5,13 +5,13 @@ class Penulis_model extends CI_Model
 
     public function get_user_data($email)
     {
-        // Mengambil data pengguna berdasarkan email
+        // Mengambil data pengguna berdasarkan email dari tabel 'penulis'
         return $this->db->get_where($this->PENULIS_USER, ['email' => $email])->row_array();
     }
 
     public function check_user($email)
     {
-        // Memeriksa keberadaan pengguna berdasarkan email
+        // Memeriksa keberadaan pengguna berdasarkan email di tabel 'penulis'
         $user = $this->db->get_where($this->PENULIS_USER, ['email' => $email])->row_array();
 
         if ($user) {
@@ -21,5 +21,17 @@ class Penulis_model extends CI_Model
             // Jika pengguna tidak ditemukan, kembalikan false
             return false;
         }
+    }
+
+    public function get_penulis()
+    {
+        // Mengambil semua data dari tabel 'penulis' dan mengembalikan dalam bentuk array objek
+        return $this->db->get('penulis')->result();
+    }
+
+    public function delete_penulis($id)
+    {
+        // Menghapus data penulis berdasarkan ID dari tabel 'penulis'
+        $this->db->delete($this->PENULIS_USER, ['nip_m' => $id]);
     }
 }
